b6aed8c2939526262f0a023be1713ca4f8e21a2c
fengdai <toxic.dai@gmail.com>
2014-11-03 21:47:26 +0800

    Fix issue when use appcompat-v7:20.

---
+++ alertdialogpro-theme-holo/src/com/alertdialogpro/holo/internal/NoTintCheckedTextView.java
@@ -0,0 +1,22 @@
+package com.alertdialogpro.holo.internal;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.CheckedTextView;
+
+/**
+ * This class is used to avoid the appcompat-v7:20 to replace CheckedTextView with TintCheckedTextView.
+ */
+public class NoTintCheckedTextView extends CheckedTextView {
+    public NoTintCheckedTextView(Context context) {
+        super(context);
+    }
+
+    public NoTintCheckedTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public NoTintCheckedTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
