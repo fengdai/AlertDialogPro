4dc87a24dfc68b10b0a583db0f309c05e0564d86
fengdai <toxic.dai@gmail.com>
2014-11-13 17:16:29 +0800

    Bug fix #11.

--- alertdialogpro-core/src/com/alertdialogpro/internal/AlertController.java
+++ alertdialogpro-core/src/com/alertdialogpro/internal/AlertController.java
@@ -190,11 +190,6 @@ public class AlertController {
     public void installContent() {
         /* We use a custom title so never request a window title */
         mWindow.requestFeature(Window.FEATURE_NO_TITLE);
-
-        if (mView == null || !canTextInput(mView)) {
-            mWindow.setFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM,
-                    WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
-        }
         mWindow.setContentView(mAlertDialogLayout);
         setupView();
     }
